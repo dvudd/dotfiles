06f1c24e16a536382827aabe7ebc5932ec86fbff
David Eriksson <david.eriksson@lundkroken.se>
2022-08-06 17:09:31 +0200

    fixat stavfel

--- fan-control.py
+++ fan-control.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python4
 
+# This script is for controlling the fans on a Dell poweredge R230, it might work on other Dell servers but use on your own risk!
+# The script assumes that you use linux, some modifications might be needed, ipmitools need to be installed.
+
 import os
 import sys
 import time
@@ -38,7 +41,7 @@ def main():
                 else:
                     # Set fan speed to 20%
                     if fan_speed == 1:
-                        print("CPU TEMP: " + str(cpu_temp) + "°C --Setting fan speed to 20%")
+                        print("CPU TEMP: " + str(cpu_temp) + "°C -- Setting fan speed to 20%")
                         fan_speed=0
                         os.system('ipmitool raw 0x30 0x30 0x02 0xff 0x14')
             time.sleep(5)
