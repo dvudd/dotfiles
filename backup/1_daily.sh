d62733784741bc21e5a06147cb1d4fda809b2035
David Eriksson <david.eriksson@lundkroken.se>
2022-09-25 23:49:58 +0200

    Flyttat backupskript till en egen mapp

---
+++ backup/1_daily.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# SOURCE: https://github.com/borgbackup/borg/blob/master/docs/quickstart.rst
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Settings
+export BORG_REPO=/mnt/data/backup
+export BORG_PASSPHRASE="gpg --decrypt /etc/backups/borg.gpg"
+archive_name=$(date +$HOSTNAME"_%d-%m-%Y")
+
+# Helpers and error handling:
+# Note: $XMPP_TARGET is a global variable leading to my XMPP address
+info() { logger -t "backup" -f /var/log/backup.log "$*" >&2; }
+xmpp() { echo "$*" | sendxmpp --tls-ca-path="/etc/ssl/certs" -t -n $XMPP_TARGET}
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+info "Daily backup: Starting"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+borg create                         \
+    --verbose                       \
+    --filter AME                    \
+    --list                          \
+    --stats                         \
+    --show-rc                       \
+    --compression lz4               \
+    --exclude-caches                \
+    --exclude-from '/etc/backups/exclude-list' \
+                                    \
+    ::$archive_name                 \
+    /etc                            \
+    /home                           \
+    /root                           \
+    /var                            \
+    /usr/local/bin                  \
+    /usr/local/sbin                 \
+    /srv                            \
+    /opt
+
+backup_exit=$?
+
+info "Daily backup: Pruning repository"
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}_*' globbing is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machines' archives also:
+
+borg prune                          \
+    --list                          \
+    --glob-archives '{hostname}_*'  \
+    --show-rc                       \
+    --keep-daily    7               \
+    --keep-weekly   4               \
+    --keep-monthly  6
+
+prune_exit=$?
+
+# actually free repo disk space by compacting segments
+
+info "Daily backup: Compacting repository"
+
+borg compact
+
+compact_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+global_exit=$(( compact_exit > global_exit ? compact_exit : global_exit ))
+
+if [ ${global_exit} -eq 0 ]; then
+    info "Daily backup: Finished successfully"
+elif [ ${global_exit} -eq 1 ]; then
+    info "Daily backup: Finished with warnings!"
+    xmpp "Daily backup of $HOSTNAME finished with warnings"
+else
+    info "Daily backup: Finished with errors!!"
+    xmpp "Daily backup of $HOSTNAME finished with errors"
+fi
+
+exit ${global_exit}
